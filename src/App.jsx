3643775531ac325414a02b612a5c52ce8a3bd9e9
Randall Reichenberger <randall.reichenberger1@gmail.com>
2024-06-14 10:48:02 -0500

    Created first home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import './App.css'
+import Routing from './Routing'
 
 function App() {
  return (
     <>
-      
+      <Routing />
     </>
   )
 }
