ecd7cbeda748f33fb9ee6fb12323f2ad8802a4f5
Randall Reichenberger <randall.reichenberger1@gmail.com>
2024-06-14 10:38:51 -0500

    Add Component/Routing Files

---
+++ src/Routes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+const Routes = () => {
+
+    <BrowserRouter> 
+        
+        <Routes>
+
+        </Routes>
+    </BrowserRouter>
+}
+
+export default Routes
